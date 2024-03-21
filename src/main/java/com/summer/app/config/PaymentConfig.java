09a9d476f228c7d0d111abc598a5fabf45ac053a
dirokim <dirokim7@gmail.com>
2024-03-21 14:34:10 +0900

    fix jstl

---
+++ src/main/java/com/summer/app/config/PaymentConfig.java
@@ -0,0 +1,13 @@
+package com.summer.app.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaymentConfig {
+
+	public PaymentConfig() {
+		// TODO Auto-generated constructor stub
+	
+	}
+	
+}
---
+++ src/main/java/com/summer/app/product/ProductController.java
@@ -0,0 +1,13 @@
+package com.summer.app.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+	
+	@GetMapping("List")
+	public void getList() throws Exception {
+		
+	}
+}
